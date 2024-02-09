e2d487e37dbfd3380f66eda334e07957d1f29cd7
DenisBilokon <Superden994@gmail.com>
2024-02-09 17:36:34 +0200

    Добавьте файлы проекта.

---
+++ SqlPractise/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlPractise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var context = new MyDbContext())
+            {
+                var group = new Group()
+                {
+                    Name = "Ramstein",
+                    Year = 1994
+                };
+
+                context.Groups.Add(group);
+                context.SaveChanges();
+
+                Console.WriteLine($"id: {group.Id}, name: {group.Name}, year: {group.Year}");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ SqlPractise/Song.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SqlPractise
+{
+    public class Song
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int GroupId {  get; set; }
+
+        public virtual ICollection<Song> Songs { get; set;}
+
+        public virtual Group Group { get; set; }
+    }
+}
